22bfe4a66ccccb94d4ddd764b0d8850e2970eac0
Magdeline <magdeline0512@gmail.com>
2019-08-17 15:24:05 +1200

    Return array of budgets

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/budgets', function(req, res, next) {
+  res.send([
+    { title: 'Rent', amount: 200 },
+    { title: 'Food', amount: 30 }
+  ]);
 });
 
 module.exports = router;
